e46d95ac4b055b81ae9e1219df7a9c1fbb20d0e9
Souravroy99 <souravroy200418@gmail.com>
2024-10-09 13:50:40 +0530

    Need to work on UI

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,15 +1,15 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
-  try 
-  {
+  try {
     const { username, email, password } = req.body;
 
     const hashPassword = await bcrypt.hash(password, 10);
     console.log(hashPassword);
 
-    const newUser = await prisma.user.create({ 
+    const newUser = await prisma.user.create({
       data: {
         username,
         email,
@@ -17,46 +17,54 @@ export const register = async (req, res) => {
       },
     });
 
-    console.log(newUser) ;
-    res.status(201).json({message: "User created successfully"}); 
-  } 
-  catch (error) 
-  {
+    console.log(newUser);
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
     console.error("Error registering user:", error);
     res.status(500).json({ message: "Failed to register user" });
   }
 };
 
-
-
 export const login = async (req, res) => {
-  const {username, password} = req.body ;
-  
-  try{
+  const { username, password } = req.body;
+
+  try {
     const user = await prisma.user.findUnique({
-      where: {username: username}
-    })
+      where: { username: username },
+    });
 
-    if(!user) res.status(401).json({message: "Invalid Credentails!"}) ;
+    if (!user) res.status(401).json({ message: "Invalid Credentails!" });
 
-    const isPasswordValid = await bcrypt.compare(password, user.password) ;
-    
-    if(!isPasswordValid) res.status(401).json({message: "Invalid Credentails!"}) ;
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    const age = 1000 * 60 * 60 * 24 * 7 ; // miliseconds
+    if (!isPasswordValid)
+      res.status(401).json({ message: "Invalid Credentails!" });
 
-    res.cookie("test", "myValue", {
-      httpOnly: true,
-      secure: true,
-      maxAge: age,
-    }).status(200).json({message: "Login successfully"}) ;
-  }
-  catch(err) {
-    console.log(err) ;
-    res.status(500).json({message: "Failed to login!"}) ;
+    const age = 1000 * 60 * 60 * 24 * 7; // 7 Days in miliseconds 
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+
+    // Using cookies 
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        maxAge: age,
+      })
+      .status(200)
+      .json({ message: "Login successfully Done" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to login!" });
   }
 };
 
 export const logout = (req, res) => {
-  res.send("Logout");
+  res.clearCookie("token").status(200).json({ message: "Logout Successfully Done" });
 };
