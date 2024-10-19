6c8197572f0b4e4dd26b6d4dddc9ef025648ad5f
Souravroy99 <souravroy200418@gmail.com>
2024-10-20 01:00:19 +0530

    Test Route is complete

--- api/controllers/test.controllers.js
+++ api/controllers/test.controllers.js
@@ -1,17 +1,41 @@
-import jwt from "jsonwebtoken" ;
+import jwt from "jsonwebtoken";
 
-export const shouldBeLoggedIn = async(req, res) => {
-    const token = req.cookies.token ;
+export const shouldBeLoggedIn = async (req, res) => {
+  const token = req.cookies.token;
 
-    if(!token) res.status(401).json({message: "Not Authenticated!"}) ;
+  if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
-    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {  // Need to understand this
-        res.status(403).json({message: "User have invalid token!"}) ;
-    })
+  // jwt.verify always takes 3 arguments
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    if (err) {
+      return res.status(403).json({ message: "User have invalid token!" });
+    }
 
-    res.status(200).json({message: "User is Authenticated"});
-}
+    console.log(payload);
+    res.status(200).json({ message: "User is Authenticated" });
+  });
+};
 
-export const shouldBeAdmin = async(req, res) => {
-    
-}
+export const shouldBeAdmin = async (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Not Authenticated!" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    // Check if there was an error during token verification
+    if (err) {
+      return res.status(403).json({ message: "User has an invalid token!" });
+    }
+
+    if (!payload.isAdmin) {
+      return res.status(403).json({ message: "User is not an admin!" });
+    }
+
+    console.log(payload);
+
+    // If the user is an admin, send the success response
+    return res.status(200).json({ message: "User is Admin" });
+  });
+};
--- api/routes/test.route.js
+++ api/routes/test.route.js
@@ -1,9 +1,9 @@
 import express from "express" ;
-import { shouldBeLoggedIn, shouldBeAdmin } from "../controllers/test.controllers";
+import { shouldBeLoggedIn, shouldBeAdmin } from "../controllers/test.controllers.js";
 
 const router = express.Router() ;
 
 router.get("/should-be-logged-in", shouldBeLoggedIn) ;
 router.get("/should-be-admin", shouldBeAdmin) ;
 
-export default router ;
+export default router ; 
